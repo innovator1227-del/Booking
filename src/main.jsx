f50f7f2e95dbc19c81a34cb7745c8f9897195bd9
innovator1227-del <innovator1227@gmail.com>
2026-02-25 22:03:40 +0300

    Replace BrowserRouter with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
   <App />
-  </BrowserRouter>
+  </HashRouter>
 )
